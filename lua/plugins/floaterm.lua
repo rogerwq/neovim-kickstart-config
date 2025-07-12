d404fe4278e7be7e02baa31d8631853d7f09b943
Qin Wan <rogerwq@gmail.com>
2025-07-12 15:23:04 +0900

    add floaterm

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,8 +1,8 @@
 return {
   'voldikss/vim-floaterm',
   config = function()
-    vim.keymap.set('n', "t", ":FloatermToggle myfloat<CR>")
-    vim.keymap.set('t', "jk", "<C-\\><C-n>:q<CR>")
-    vim.keymap.set('t', "kj", "<C-\\><C-n>:q<CR>")
-  end
+    vim.keymap.set('n', 't', ':FloatermToggle myfloat<CR>')
+    vim.keymap.set('t', 'jk', '<C-\\><C-n>:q<CR>')
+    vim.keymap.set('t', 'kj', '<C-\\><C-n>:q<CR>')
+  end,
 }
